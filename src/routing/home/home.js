b1868cbf163ee32e297eb423b2045236be6f1b3c
Raymond Cox <raymondc1911@gmail.com>
2019-04-23 20:41:19 -0400

    Playing around

--- src/routing/home/home.js
+++ src/routing/home/home.js
@@ -1,9 +1,50 @@
 import React, { Component } from "react";
+
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+      number: 4,
+      phrase: "hi five",
+    }
+  }
+  
+  componentDidMount() {
+    this.sanitizeData();
+  }
+
+  fetchData = (url) => {
+     return fetch(url)
+      .then(response => {return response.json()})
+      .catch(error => console.error(error));
+
+  }
+
+  sanitizeData = (url = `people/1/`) => {
+    console.log(`https://swapi.co/api/${url}`)
+    this.fetchData(`https://swapi.co/api/${url}`).then(result => {this.setState({
+      data: result
+    })});
+  }
+
   render() {
+    const { data } = this.state;
+    console.log(this.state);
+    console.log(data);
     return (
       <div>
-        <h1>Home</h1>
+        {!!data && !!data.name && (
+        <div>
+          <h1>Name: {data.name}</h1>
+            <p>List of films: </p>
+              <ol>{data.films.map((film, index) => { 
+                return <li key={index}><a href={film}>{film}</a></li>
+                })}
+              </ol>
+            
+        </div>
+        )}
       </div>
     );
   }
